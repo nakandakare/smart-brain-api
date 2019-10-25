e076834ea9ee0d4713951e5ffd630a8a74bf874a
Kevin <kevin.nakandakare@gmail.com>
2019-10-25 18:15:50 -0300

    update

--- server.js
+++ server.js
@@ -19,8 +19,7 @@ const db = knex({
 });
 
 const database = {
-    users: [
-        {
+    users: [{
             id: '123',
             name: 'John',
             password: 'cookies',
@@ -37,98 +36,94 @@ const database = {
             joined: new Date(),
         }
     ],
-    login: [
-        {
-            id: '987',
-            hash: '',
-            email: 'john@gmail.com'
-        }
-    ]
-}  
- 
-app.get('/',(req,res)=>{
-    res.send(database.users);
+    login: [{
+        id: '987',
+        hash: '',
+        email: 'john@gmail.com'
+    }]
+}
+
+app.get('/', (req, res) => {
+    res.send("hello");
 })
 
-app.post('/signin',(req,res)=>{
-    db.select('email','hash').from('login')
-    .where('email', '=' , req.body.email)
-    .then(data => {
-        const isValid = bcrypt.compareSync(req.body.password, data[0].hash)
-        if(isValid){
-            return db.select('*').from('users')
-            .where('email','=',req.body.email)
-            .then(user => {
-                res.json(user[0])
-            })
-            .catch(err => res.status(400).json("Error"));
-        } else {
-            res.status(400).json('No coincide la contraseña');
-        }
-    })
-    .catch(err => res.status(400).json('No coincide la contraseña o email'))
+app.post('/signin', (req, res) => {
+    db.select('email', 'hash').from('login')
+        .where('email', '=', req.body.email)
+        .then(data => {
+            const isValid = bcrypt.compareSync(req.body.password, data[0].hash)
+            if (isValid) {
+                return db.select('*').from('users')
+                    .where('email', '=', req.body.email)
+                    .then(user => {
+                        res.json(user[0])
+                    })
+                    .catch(err => res.status(400).json("Error"));
+            } else {
+                res.status(400).json('No coincide la contraseña');
+            }
+        })
+        .catch(err => res.status(400).json('No coincide la contraseña o email'))
 })
 
-app.post('/register',(req,res)=>{
-    const {email,name,password} = req.body; //destructigturing
-    if(!email || !name || !password){
+app.post('/register', (req, res) => {
+    const { email, name, password } = req.body; //destructigturing
+    if (!email || !name || !password) {
         return res.status(400).json("Error en la información ingresada");
     }
-    const hash = bcrypt.hashSync(password,0);
-    db.transaction(trx => {//si uno falla, todo falla (ya q usamos 2 tablas login y register)
-        trx.insert({
-            hash: hash,
-            email: email
+    const hash = bcrypt.hashSync(password, 0);
+    db.transaction(trx => { //si uno falla, todo falla (ya q usamos 2 tablas login y register)
+            trx.insert({
+                    hash: hash,
+                    email: email
+                })
+                .into('login')
+                .returning('email')
+                .then(loginEmail => {
+                    return trx('users')
+                        .returning('*')
+                        .insert({
+                            email: loginEmail[0],
+                            name: name,
+                            joined: new Date()
+                        })
+                        .then(user => {
+                            res.json(user[0]); //retorna todo de la tabla ya q hay returning(*)
+                        })
+                })
+                .then(trx.commit) //si funciona todo, commit
+                .catch(trx.rollback) //sino rollback
         })
-        .into('login')
-        .returning('email')
-        .then(loginEmail => { 
-            return  trx('users')
-                    .returning('*')
-                    .insert({
-                        email:loginEmail[0],
-                        name: name,
-                        joined: new Date()
-                    })
-                    .then(user => {
-                        res.json(user[0]); //retorna todo de la tabla ya q hay returning(*)
-                    })
-        })
-        .then(trx.commit) //si funciona todo, commit
-        .catch(trx.rollback) //sino rollback
-    }) 
         .catch(err => res.status(400).json("No se puede conectar"))
-    //cuando se registra 2 veces el mismo mail, agarra un erro desde basededatos.
+        //cuando se registra 2 veces el mismo mail, agarra un erro desde basededatos.
 })
 
-app.get('/profile/:id',(req,res)=>{
+app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
     db.select('*').from('users').where({
-        id: id,
-    })
-    .then(user => {
-        if(user.length){
-            res.json(user[0]);
-        } else {
-            res.status(400).json('No existe el usuario');
-        }
-    })
-   
+            id: id,
+        })
+        .then(user => {
+            if (user.length) {
+                res.json(user[0]);
+            } else {
+                res.status(400).json('No existe el usuario');
+            }
+        })
+
 })
 
-app.put('/image',(req,res)=>{
+app.put('/image', (req, res) => {
     const { id } = req.body;
-    db('users').where('id','=',id)
-    .increment('entries', 1)
-    .returning('entries')
-    .then(entries => {
-        res.json(entries[0]);
-    })
-    .catch(err => res.status(400).json(err));   
+    db('users').where('id', '=', id)
+        .increment('entries', 1)
+        .returning('entries')
+        .then(entries => {
+            res.json(entries[0]);
+        })
+        .catch(err => res.status(400).json(err));
 })
 
-app.listen(3000, ()=>{
+app.listen(3000, () => {
     console.log('working');
 });
-
-
